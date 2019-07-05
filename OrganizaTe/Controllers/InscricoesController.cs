9f04a5885cc053ec15cc9d6a97baa409065c5a08
Pedro Vinha <pa.v.vinha@gmail.com>
2019-07-05 22:29:14 +0100

    Starting InscricoesController

--- OrganizaTe/Controllers/InscricoesController.cs
+++ OrganizaTe/Controllers/InscricoesController.cs
@@ -1,11 +1,30 @@
-﻿using System;
+﻿using OrganizaTe.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Web.Mvc;
 
 namespace OrganizaTe.Controllers
 {
-    public class InscricoesController
+    public class InscricoesController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Cursos/Create
+        public ActionResult Create()
+        {
+            return View(new Inscricoes());
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
     }
 }
