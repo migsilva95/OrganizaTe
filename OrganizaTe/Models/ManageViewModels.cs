919d0f70d797056a6d4ae9fc48fb2113a2d3e641
Pedro Vinha <pa.v.vinha@gmail.com>
2019-07-09 19:03:41 +0100

    Changing the forms to Portuguese

--- OrganizaTe/Models/ManageViewModels.cs
+++ OrganizaTe/Models/ManageViewModels.cs
@@ -28,14 +28,14 @@ namespace OrganizaTe.Models
     public class SetPasswordViewModel
     {
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "O {0} tem que ser pelo menos {2} caracteres.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "New password")]
+        [Display(Name = "Nova password")]
         public string NewPassword { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm new password")]
-        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        [Display(Name = "Confirme a nova password")]
+        [Compare("NewPassword", ErrorMessage = "A nova password e a sua confirmação não são iguais.")]
         public string ConfirmPassword { get; set; }
     }
 
@@ -43,18 +43,18 @@ namespace OrganizaTe.Models
     {
         [Required]
         [DataType(DataType.Password)]
-        [Display(Name = "Current password")]
+        [Display(Name = "Password atual")]
         public string OldPassword { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "O {0} tem que ser pelo menos {2} caracteres.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "New password")]
+        [Display(Name = "Nova password")]
         public string NewPassword { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm new password")]
-        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        [Display(Name = "Confirme a nova password")]
+        [Compare("NewPassword", ErrorMessage = "A nova password e a sua confirmação não são iguais.")]
         public string ConfirmPassword { get; set; }
     }
 }
